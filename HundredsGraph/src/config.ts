d2a7a436a1b594405bb78d1c3ab688f36e26d0e1
Kelly <38844676+KEMProjects@users.noreply.github.com>
2023-09-13 16:46:23 +0200

    fix size of the window

--- HundredsGraph/src/config.ts
+++ HundredsGraph/src/config.ts
@@ -3,10 +3,10 @@ import Phaser from 'phaser';
 export default {
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#33A5E7',
+  backgroundColor: '#9ECBE5',
   scale: {
-    width: 800,
-    height: 600,
+    width: 1440,
+    height: 900,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
